9a3596a805cfe706c26b94302f6c55a67fb3a147
xianzhou <69417747+xianzhou127@users.noreply.github.com>
2024-03-14 17:45:24 +0800

    Add files via upload

--- atr_pkg/atr_pkg/Motor_node.py
+++ atr_pkg/atr_pkg/Motor_node.py
@@ -5,6 +5,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+from robot_msgs.srv import MoveRobot
+
 class AtrNode(Node):
     '''
     define a class of atr node
@@ -12,11 +14,23 @@ class AtrNode(Node):
     def __init__(self,name):
         super().__init__(name)
         self.get_logger().info("subscriber motor_node online")
+        self.pose = 0
         self.sub_pos = self.create_subscription(String,"Position",self.__sub_callback,qos_profile=10)
-
+        self.moverobot_srv_ = self.create_service(MoveRobot,"move_robot",self.__moverobot_callback)
     def __sub_callback(self,msg):
         self.get_logger().info(msg.data)
 
+    def __moverobot_callback(self,request,response):
+        '''
+        服务端回调函数
+        获得前进距离，并返回现在位置
+        request\response-MoveRobot类型
+        '''
+        self.get_logger().info("get requestions,moving forward %f meters"%request.distance)
+        self.pose += request.distance
+        response.pose = self.pose
+        return response
+    
 def main(args=None):
     rclpy.init(args=args)
     Motor_node = AtrNode("Motor_node")
